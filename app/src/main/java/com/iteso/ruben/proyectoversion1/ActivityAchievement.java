40e225adaf8c7525e559a640532fb101c3098099
Elizabeth <elizabeth24gl@icloud.com>
2017-11-02 18:03:03 -0600

    mas mejoras en layouts, botones y estilos

--- app/src/main/java/com/iteso/ruben/proyectoversion1/ActivityAchievement.java
+++ app/src/main/java/com/iteso/ruben/proyectoversion1/ActivityAchievement.java
@@ -3,11 +3,14 @@ package com.iteso.ruben.proyectoversion1;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ListView;
 
 public class ActivityAchievement extends AppCompatActivity {
 
     private ListView listView;
+    protected ImageButton back_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +18,7 @@ public class ActivityAchievement extends AppCompatActivity {
         setContentView(R.layout.activity_achievement);
 
         listView = (ListView) findViewById(R.id.list);
+        back_button = (ImageButton) findViewById(R.id.activity_a_back_button);
 
         Resources res = getResources();
         String[] goal = res.getStringArray(R.array.myGoals);
@@ -23,6 +27,14 @@ public class ActivityAchievement extends AppCompatActivity {
         listView.setAdapter(adapter);
 
 
+        back_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Intent select_intent = new Intent(ActivitySleep.this, ActivityHome.class);
+                //startActivity(select_intent);
+                finish();
+            }
+        });
        // ArrayAdapter adapter = new ArrayAdapter<String>(this,
        // R.layout.achieves_list_item, R.id.textView1,goal);
 
