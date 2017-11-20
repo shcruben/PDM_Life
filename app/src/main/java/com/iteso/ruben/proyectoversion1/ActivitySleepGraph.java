f3d2eac3cf2c90d9f428553f4c44a23849542797
rubenher <shc.ruben@gmail.com>
2017-11-20 11:42:16 -0600

    * Grafica sueño terminada * Animaciones agregadas

--- app/src/main/java/com/iteso/ruben/proyectoversion1/ActivitySleepGraph.java
+++ app/src/main/java/com/iteso/ruben/proyectoversion1/ActivitySleepGraph.java
@@ -12,8 +12,18 @@ import android.widget.Toast;
 import com.google.gson.internal.LinkedTreeMap;
 import com.jawbone.upplatformsdk.api.ApiManager;
 import com.jawbone.upplatformsdk.utils.UpPlatformSdkConstants;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.Series;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
 
 import retrofit.Callback;
 import retrofit.RetrofitError;
@@ -23,6 +33,8 @@ public class ActivitySleepGraph extends AppCompatActivity {
 
     private String mAccessToken;
     private ImageButton back_button;
+    private ArrayList<DataPoint> sleepsTime;
+    private GraphView graphView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +52,8 @@ public class ActivitySleepGraph extends AppCompatActivity {
                 UpPlatformSdkConstants.API_VERSION_STRING,
                 null,
                 sleepEventListCallbackListener);
+
+        graphView = (GraphView) findViewById(R.id.activity_sleep_graph_graph);
 
         back_button = (ImageButton) findViewById(R.id.activity_sleep_graph_back_button);
 
@@ -60,9 +74,47 @@ public class ActivitySleepGraph extends AppCompatActivity {
             LinkedTreeMap<String, LinkedTreeMap> received = (LinkedTreeMap) o;
             LinkedTreeMap<String, ArrayList> data = received.get("data");
             ArrayList< LinkedTreeMap <String, Object> > items = data.get("items");
+
+            sleepsTime = new ArrayList<>();
+            double i = 0;
+
             for (LinkedTreeMap<String, Object> l : items) {
                 Log.e("MoveEventList ",  (String) l.get("title"));
+
+                Double timeCreated = (Double) l.get("time_created");
+                Double timeCompleted = (Double) l.get("time_completed");
+                Double totalSleep = (timeCompleted - timeCreated) / 3600;
+                Date date = new Date( (long) (timeCompleted *1000) );
+
+                sleepsTime.add( new DataPoint(i, totalSleep) );
+                i++;
             }
+
+            Collections.sort(sleepsTime, new Comparator<DataPoint>() {
+                @Override
+                public int compare(DataPoint o1, DataPoint o2) {
+                    return o1.getX() == o2.getX() ? 0 : o1.getX() > o2.getX() ? 1 : -1;
+                }
+            });
+            sleepsTime.add(sleepsTime.get(sleepsTime.size()-1));
+            DataPoint[] sleepTimeDP  = new DataPoint[sleepsTime.size()];
+            sleepsTime.toArray(sleepTimeDP);
+
+            MyAnimPlot myAnimPlot = new MyAnimPlot(graphView, sleepTimeDP);
+            myAnimPlot.show();
+
+            graphView.setTitle("Sleep Time Graph");
+
+//            List<Series> list = graphView.getSeries();
+//
+//            for(Series<DataPoint> series : list){
+//                Iterator<DataPoint> iterator = series.getValues(series.getHighestValueX(), series.getHighestValueX());
+//                while(iterator.hasNext()){
+//                    DataPoint dataPoint = iterator.next();
+//                    Log.e("ASDFG", dataPoint.toString());
+//                }
+//            }
+//
         }
 
         @Override
